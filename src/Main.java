a060212f45023b120416f13fc0f34592b3d0f64d
Seeker <tzzxy7@foxmail.com>
2019-12-27 14:36:20 +0800

    连续子数组

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,34 @@
-import java.util.Stack;
+import com.sun.org.apache.regexp.internal.RE;
+
+import java.util.*;
 
 public class Main {
 
     static class Solution{
+        /**
+         * 字符串的排列
+         * @param str
+         * @return
+         */
+        public ArrayList<String> Permutation(String str) {
+            HashSet<String> res=new HashSet<>();
+            if (str.length()==0)
+                return new ArrayList<>();
+            else if (str.length()==1){
+                res.add(str);
+                return new ArrayList<>(res);
+            }else{
+                for (int i=0;i<str.length();i++){
+                    String substr = str.substring(0,i).concat(str.substring(i+1));
+                    for (String s:Permutation(substr)){
+                        res.add(str.substring(i,i+1).concat(s));
+                    }
+                }
+                ArrayList<String> re=new ArrayList<>(res);
+                Collections.sort(re);
+                return re;
+            }
+        }
         /**
          * 数组中出现次数超过一半的数字
          * @param array
@@ -32,10 +58,54 @@ public class Main {
             }
             return (count>(array.length/2))?res:0;
         }
+
+        /**
+         * 最小的k个数
+         * @param input
+         * @param k
+         * @return
+         */
+        public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+            Arrays.sort(input);
+            ArrayList<Integer> res=new ArrayList<>();
+            if (input.length==0||k==0||k>input.length)
+                return res;
+            for (int i=0;i<k;i++){
+                res.add(input[i]);
+            }
+            return res;
+        }
+        /**
+         * 连续子数组最大和
+         * @param array
+         * @return
+         */
+        public int FindGreatestSumOfSubArray(int[] array) {
+            if (array.length==0)
+                return 0;
+            int res=Integer.MIN_VALUE;
+            int sum=array[0];
+            for (int i=1;i<array.length;i++){
+                if (sum+array[i]>0){
+                    sum+=array[i];
+                    res=(sum<res)?res:sum;
+                }else
+                    sum=0;
+            }
+            if (res==Integer.MIN_VALUE) {
+                for (int i:array){
+                    if (res<i)
+                        res=i;
+                }
+            }
+            return res;
+        }
     }
+
+
 
     public static void main(String[] args) {
         Solution solution=new Solution();
-        System.out.println(solution.MoreThanHalfNum_Solution(new int[]{1,2,3,2,4,2,5,2,3}));
+        System.out.println(solution.FindGreatestSumOfSubArray(new int[]{-11,-2,-3}));
     }
 }
